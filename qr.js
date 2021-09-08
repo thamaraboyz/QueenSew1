a917b9a3b04ccafe384e0a05138dad37fce955cf
thamaraboyz <87033944+thamaraboyz@users.noreply.github.com>
2021-09-08 19:48:13 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,12 +1,9 @@
-/* Codded by @Ravindu Manoj
-
-Telegram: t.me/RavinduManoj
-Facebook: https://www.facebook.com/ravindu.manoj.79
+/* Codded by @thamaraboyz
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Whats bot - Ravindu Manoj
+Whats bot - 🅽︎🅾︎ㄖ几乇
 */
 
 const chalk = require('chalk');
